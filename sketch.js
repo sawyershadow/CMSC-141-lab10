dab3cb676b073f980b1d954d5d27e4a80e48de95
sawyershadow <95491239+sawyershadow@users.noreply.github.com>
2021-12-03 12:15:27 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
-function setup() {
-  // put setup code here
+var fence;
+var polygon = [
+    {lat: 34.045303, lon: -118.334650},  // top left  
+    {lat: 34.045252, lon: -118.334462},  // top right
+    {lat: 34.045131, lon: -118.334498},  // bottom right
+    {lat: 34.045185, lon: -118.334684},  // bottom left
+];
+function setup(){
+
+    //optional options object for geoFencegeoFencePolygon
+    //fence = new geoFenceCircle(polygon, insideTheFence, 'mi', fenceOptions)
+    // fenceOptions = {
+    //   enableHighAccuracy: false,
+    //   timeout: 5000,
+    //   maximumAge: 0
+    // };
+
+    fence = new geoFencePolygon(polygon, insideTheFence, outsideTheFence, 'mi')
 }
 
-function draw() {
-  // put drawing code here
+function insideTheFence(position){
+    print("INlat: " + position.latitude);
+    print("INlong: " + position.longitude);
+    print("user is inside of the fence")
+}
+
+function outsideTheFence(position){
+    print("OUTlat: " + position.latitude);
+    print("OUTlong: " + position.longitude);
+    print("user is outside of the fence")
 }
